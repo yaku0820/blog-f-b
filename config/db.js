d1d5be4d36734748a24e43979e83f58f4d3c093b
Uuganbayar <uu80277008@gmail.com>
2023-09-13 11:34:41 +0800

    blog-front-back

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv')
+const mongoose = require("mongoose");
+const colors = require("colors");
+dotenv.config()
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log(`MONGO Connect Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
